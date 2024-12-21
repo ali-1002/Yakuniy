17e8e43c0df00a2220dcaf2e155abd5aae38141d
Ali <alijonsuvonov77@gmail.com>
2024-12-21 15:45:28 +0500

    Initial commit

---
+++ misoll/models.py
@@ -0,0 +1,3 @@
+from django.core.validators import MaxValueValidator
+from django.db import models
+# Create your models here.
---
+++ misoll/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import CountriesModel
+from django.db.models import Q
+# from .forms import 
